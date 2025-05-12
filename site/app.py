017c2c572007de0abda5853f471916687b08e6e7
LISV <arsenemaxencezs@gmail.com>
2025-05-12 16:59:44 +0200

    remplissage du tableau via la bd mysql

--- site/app.py
+++ site/app.py
@@ -1,20 +1,39 @@
-import mysql.connector
 from flask import Flask, render_template
+import MySQLdb
 
 app = Flask(__name__)
 
+
 def get_db_connection():
-    return mysql.connector.connect(host='localhost',user='root',password='root',database='lisv')
+    try:
+        print(" Tentative de connexion à MySQL...")
+        conn = MySQLdb.connect(
+            host='127.0.0.1',
+            user='root',
+            password='root',
+            database='lisv',
+            port=3306,
+            auth_plugin='mysql_native_password'
+        )
+        print(" Connexion MySQL réussie!")
+        return conn
+    except Exception as e:
+        print(" erreur de con:")
+        return None
+
 
 @app.route('/')
 def index():
     conn = get_db_connection()
+    if conn is None:
+        return " Échec de connexion à MySQL"
     cursor = conn.cursor()
     cursor.execute('SELECT * FROM info')
     infos = cursor.fetchall()
     cursor.close()
     conn.close()
-    return render_template('page1.html', infos=infos)
+
+    return render_template('index.html', infos=infos)
 
 if __name__ == '__main__':
     app.run(debug=True)
